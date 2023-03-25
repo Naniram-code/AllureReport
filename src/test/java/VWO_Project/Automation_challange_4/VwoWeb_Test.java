49af9e7df27fddfe3964802a1795809a8c61449d
Naniram-code <NaniramBhujel@gmail.com>
2023-03-24 22:33:32 -0400

    second commit

--- src/test/java/VWO_Project/Automation_challange_4/VwoWeb_Test.java
+++ src/test/java/VWO_Project/Automation_challange_4/VwoWeb_Test.java
@@ -1,6 +1,7 @@
-package VWO_Project;
+package VWO_Project.Automation_challange_4;
 
-import org.example.Main;
+
+import io.qameta.allure.Description;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -33,21 +34,22 @@ public class VwoWeb_Test {
         driver.get("https://app.vwo.com");
         driver.manage().window().maximize();
     }
-    @Test(priority =1,enabled =false)
+    @Test(priority =1,enabled =true)
+    @Description("Create new user and verification ")
     public void NewRegistration() throws InterruptedException {
         Actions actions = new Actions(driver);
         driver.findElement(By.linkText("Start a free trial")).click();
         WebElement emailadd=driver.findElement(By.tagName("input"));
         //emailadd.sendKeys("Abc4321@gmail.com");//(1st user created)
         //emailadd.sendKeys("Abcd4321@gmail.com");//(2nd user created)
-        emailadd.sendKeys("ARbcd34321@gmail.com");//(3nd user created)
+        emailadd.sendKeys("Eabcdefgh@gmail.com");//(3nd user created)
         driver.findElement(By.tagName("button")).click();
         Thread.sleep(5000);
        WebElement name=driver.findElement(By.xpath("//*[@id='page-v1-fname']"));
         Thread.sleep(5000);
-        actions.sendKeys(name,"Jacksion").build().perform();
+        actions.sendKeys(name,"abcd$#$").build().perform();
         WebElement Lastname=driver.findElement(By.xpath("//*[@id='page-v1-lname']"));
-        actions.sendKeys(Lastname,"Zazz").build().perform();
+        actions.sendKeys(Lastname,"ABC").build().perform();
 
         WebElement Clist=driver.findElement(By.className("iti__flag-container"));
         actions.moveToElement(Clist).click().build().perform();
@@ -56,9 +58,9 @@ public class VwoWeb_Test {
         actions.moveToElement(Ncountry).click().build().perform();
 
         WebElement PhoneNum=driver.findElement(By.xpath("//*[@id='page-v1-pnumber']"));
-        actions.sendKeys(PhoneNum,"234576946").build().perform();
+        actions.sendKeys(PhoneNum,"203576946").build().perform();
         WebElement Psw=driver.findElement(By.xpath("//*[@id='page-v1-pwd']"));
-        actions.sendKeys(Psw,"abcedrg@#$$$65").build().perform();
+        actions.sendKeys(Psw,"abcedrg@#$$$(*").build().perform();
         WebElement clickbutton=driver.findElement(By.tagName("button"));
         actions.moveToElement(clickbutton).doubleClick();
         String ActualTitle=driver.getTitle();
@@ -71,6 +73,7 @@ public class VwoWeb_Test {
     }
 
     @Test(priority = 2)
+    @Description("Negetive  login  verification ")
     public void loginNegetiveTest() throws InterruptedException {
         WebElement email = driver.findElement(By.id("login-username"));
         email.sendKeys("bb0@esiix.ccom");
@@ -90,12 +93,11 @@ public class VwoWeb_Test {
         driver.close();}
 
     @Test(priority = 3)
+    @Description("positive login  verification ")
     public void loginPositiveTest() throws InterruptedException {
         WebElement email = driver.findElement(By.id("login-username"));
-        //email.sendKeys("vwouser@gmail.com");//(User 1)
         email.sendKeys("Abc4321@gmail.com");
         WebElement password = driver.findElement(By.name("password"));
-        //password.sendKeys("!@#$%^&*()Abc");(psw user 1)
         password.sendKeys("abcedrg@#$$$6");
         driver.findElement(By.id("js-login-btn")).click();
 
@@ -115,6 +117,7 @@ public class VwoWeb_Test {
         Assert.assertEquals(ActualloginpageTitle,ExpectedloginpageTitle);
         driver.close();}
     @Test(priority = 3)
+    @Description("Valid  user login and logout verification ")
     public void VwoAssignment() throws InterruptedException {
         Actions actions= new Actions(driver);
         WebElement email = driver.findElement(By.id("login-username"));
@@ -125,28 +128,38 @@ public class VwoWeb_Test {
 
         WebElement checkbox=driver.findElement(By.xpath("//*[@class='checked-login-icon text--highlight']"));
               actions.moveToElement(checkbox).click().build().perform();
-        Thread.sleep(5000);
 
-        WebElement element=driver.findElement(By.id("js-login-btn"));
-        actions.moveToElement(element).click().build().perform();
-        Thread.sleep(5000);
+        WebElement signin=new WebDriverWait(driver, Duration.ofSeconds(5)) //Explicit Wait
+                .until(ExpectedConditions.presenceOfElementLocated(By.id("js-login-btn")));
+                 actions.moveToElement(signin).doubleClick().build().perform();
 
-        WebElement text=driver.findElement(By.xpath("//p[@class='page-sub-title']"));
-        String ActuallDashbordPageText = text.getText();
-        System.out.println(ActuallDashbordPageText);
+
+
+        WebElement DashboardText=new WebDriverWait(driver, Duration.ofSeconds(10))
+               .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[@class='page-sub-title']")));
+
+        String ActuallDashbordPageText = DashboardText.getText();
+        System.out.println("Login success Page Text:"+ActuallDashbordPageText);
         String ExpectedlDashbordPageText="Hi vwo1 bwo2, here's an overview of your experience optimization journey";
-        Assert.assertEquals(ActuallDashbordPageText,ExpectedlDashbordPageText);
-        Thread.sleep(5000);
-       WebElement ss=driver.findElement(By.xpath("(//img[@alt='vwo1 bwo2'])[2]"));
-        actions.moveToElement(ss).click().build().perform();
 
-         WebElement sss=driver.findElement(By.xpath("//*[@class='menu-content js-menu-content open']/menu-content/ul/li[2]"));
-        System.out.println(sss.getText());
-        actions.moveToElement(sss).doubleClick().build().perform();
-        Thread.sleep(5000);
-        System.out.println(driver.getTitle());
-        Thread.sleep(5000);
+        Assert.assertEquals(ActuallDashbordPageText,ExpectedlDashbordPageText);
+        WebElement Usermenu=driver.findElement(By.xpath("(//img[@alt='vwo1 bwo2'])[2]"));
+        actions.moveToElement(Usermenu).click().build().perform();
+
+        WebElement logout=driver.findElement(By.xpath("//*[@class='menu-content js-menu-content open']/menu-content/ul/li[2]"));
+        boolean bb=logout.isDisplayed();
+        System.out.println(logout.getText());
+
+        actions.moveToElement(logout).doubleClick().build().perform();
+
+        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(5));
+        wait.until((ExpectedConditions.titleIs("Login - VWO")));
+        System.out.println("Sign in Page Title:"+driver.getTitle());
         driver.close();}
+
+
+
+
     @AfterTest
     public void tearDown() {
 
